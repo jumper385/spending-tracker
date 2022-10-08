bbd020a5c91f56171e2805499dbefab7da5fef8d
Henry Chen <henrychen385@gmail.com>
2022-10-08 22:58:38 +0800

    added more gif options

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     const randomGif = () => {
-        let gifs = ["UZ8rFjJvlfGrm", "12pJ8OxSWwO86Y", "67ThRZlYBvibtdF9JH", "K0fCYZRbETczFWydo0"];
+        let gifs = ["UZ8rFjJvlfGrm", "12pJ8OxSWwO86Y", "67ThRZlYBvibtdF9JH", "K0fCYZRbETczFWydo0", "xm4uYTatdDtbmCsBCw", "eTrYclI5fuEIzuTo3A",];
         let randomIndex = Math.floor(Math.random() * gifs.length);
         let gifid = gifs[randomIndex];
 
