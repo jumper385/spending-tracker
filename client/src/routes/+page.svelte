31de1388f2e417fd3cd68051130239240de5291b
Henry Chen <henrychen385@gmail.com>
2022-10-08 23:38:57 +0800

    added simpsons and donald duck gif

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     const randomGif = () => {
-        let gifs = ["UZ8rFjJvlfGrm", "12pJ8OxSWwO86Y", "67ThRZlYBvibtdF9JH", "K0fCYZRbETczFWydo0", "xm4uYTatdDtbmCsBCw", "eTrYclI5fuEIzuTo3A",];
+        let gifs = ["UZ8rFjJvlfGrm", "12pJ8OxSWwO86Y", "67ThRZlYBvibtdF9JH", "K0fCYZRbETczFWydo0", "xm4uYTatdDtbmCsBCw", "eTrYclI5fuEIzuTo3A","xT5LMYj7zNov6QhN28", "xTiTnqUxyWbsAXq7Ju"];
         let randomIndex = Math.floor(Math.random() * gifs.length);
         let gifid = gifs[randomIndex];
 
